9916c55a55836db935c1fe56f71241c9451c7d78
Sebastian Arriagada <snarriagada@uc.cl>
2021-12-20 23:03:43 -0300

    feat(main): add output handling

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ try:
 except:
     print("An exception ocurred with the .txt input")
 
-romans = ["I", "V", "X", "L", "C", "D", "M"]
-
 def main(input):
     conversion = {}
     metals_price = {}
     unit_prices = {}
+
+    output_lines = []
+
     with open(input, 'r') as file:
         for line in file:
             line = line.rstrip('\n')
@@ -21,17 +22,28 @@ def main(input):
             line = line.split(" is ")
             if is_traduction(line):
                 conversion[line[0]] = line[1]
-            if is_metal_price(line):
+            elif is_metal_price(line):
                 save_metal_price(line, metals_price)
-            if is_conversion_question(line):
+            elif is_conversion_question(line):
                 amount = line[1].split(" ")
                 roman_number = galactic_to_roman(amount, conversion)
-                roman_to_integer(roman_number)
-            if is_price_question(line):
+                result = roman_to_integer(roman_number)
+                response = line[1][:-1]+"is "+str(result)
+                output_lines.append(response)
+            elif is_price_question(line):
                 unit_prices = get_unit_metal_prices(metals_price, conversion)
                 deal_price = get_deal_price(line[1], unit_prices, conversion)
-                print(deal_price)
+                response = line[1][:-1]+"is "+str(deal_price)+" Credits"
+                output_lines.append(response)
+                #print(deal_price)
+            else:
+                output_lines.append("I have no idea what you are talking about")
+        write_output(output_lines)
 
+def write_output(output_lines):
+    with open('output.txt', 'w') as file:
+        file.write('\n'.join(output_lines))
+    return
                 
     print("------")
     print("conversion: ", conversion)
